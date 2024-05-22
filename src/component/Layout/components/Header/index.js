64463c58e7a2bc6562b421aee271d237d5089cb9
TheTrieste <triet217@gmail.com>
2024-05-22 14:49:34 +0700

    Done header raw

--- src/component/Layout/components/Header/index.js
+++ src/component/Layout/components/Header/index.js
@@ -1,14 +1,33 @@
 import classNames from 'classnames/bind';
-
 import styles from './index.module.css';
+
+import line from '../../../../assets/icon/line.svg'
+import cart from '../../../../assets/icon/cart.svg'
+import opNavIcon from '../../../../assets/icon/openNav.svg'
+import userIcon from '../../../../assets/icon/user.svg'
+import logoSmall from '../../../../assets/logo/logoSmall.png'
 
 const cx = classNames.bind(styles);
 
 function Header() {
     return (
-        <header className={cx('wrapper')}>
-            <h1>Header</h1>
-        </header>
+    <div className={`${cx('wrapper')} d-flex justify-content-between align-items-center`}>
+        <div>
+            <h5 class="mx-2"><img src={opNavIcon} alt="Navigation"/></h5>
+        </div>
+        <div className='d-flex justify-content-center flex-column' style={{paddingLeft: '150px'}}>
+            <img className={`${cx('header-logo')}`} src={logoSmall} alt="Navigation"/>
+            <label className={`${cx('header-logo-text')}`}>Việt Sắc</label>
+        </div>
+        <div className={`${cx('header-user-section')} d-flex justify-content-center align-items-center flex-row`}>
+            <div className='d-flex justify-content-center align-items-center flex-row'>
+                <p className={`${cx('user-title')} m-0 me-3`}>Say hi to us</p>
+                <img className={`${cx('header-icon')}`} src={userIcon} alt="User"/>
+            </div>
+            <img className={`mx-4`} src={line} alt="Line"/>
+            <img src={cart} alt="Cart"/>
+        </div>
+     </div>
     );
 }
 
